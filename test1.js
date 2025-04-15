09bebb0bc0f8a6195b430bc76b2146a145cbb734
Arce Nahuel <164960010+nahuarce12@users.noreply.github.com>
2025-04-15 16:28:00 -0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -8,3 +8,4 @@ function add(a, b) {
 // Example test case
 console.log("Test Case 1: add(2, 3) === 5");
 console.log(add(2, 3) === 5 ? "Passed" : "Failed");
+console.log("Second test, this is a message writed from remote");
