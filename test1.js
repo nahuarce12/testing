bfab6efb9e3c9d1625650b5d8645ab8ca97011b4
NahuelArce <nahuellarce@gmail.com>
2025-04-15 16:25:03 -0300

    pruebanro1

---
+++ test1.js
@@ -0,0 +1,10 @@
+// Basic example of a function and a test case
+
+// Function to add two numbers
+function add(a, b) {
+  return a + b;
+}
+
+// Example test case
+console.log("Test Case 1: add(2, 3) === 5");
+console.log(add(2, 3) === 5 ? "Passed" : "Failed");
