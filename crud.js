50d3aba6e87183fa38f687fd26552193335814b1
NahuelArce <nahuellarce@gmail.com>
2025-04-15 17:00:59 -0300

    addedDBConnection

--- crud.js
+++ crud.js
@@ -1,3 +1,5 @@
+const sqlite3 = require('sqlite3').verbose();
+
 // Basic CRUD operations for a "Client" entity
 
 let clients = []; // In-memory storage for clients
@@ -48,3 +50,18 @@ console.log(getClient(client2.id));
 
 deleteClient(client1.id);
 console.log(getAllClients());
+
+const db = new sqlite3.Database('./clients.db', (err) => {
+  if (err) {
+    console.error('Could not connect to database', err);
+  } else {
+    console.log('Connected to SQLite database');
+    db.run(`
+      CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT NOT NULL
+      )
+    `);
+  }
+});
