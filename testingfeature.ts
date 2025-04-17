a3cd80304c10119f7b32f08c30151a018c7d9ada
NahuelArce <nahuellarce@gmail.com>
2025-04-17 17:14:33 -0300

    final testonthisfeature

--- testingfeature.ts
+++ testingfeature.ts
@@ -5,3 +5,13 @@ export function sayHello(name: string): string {
 export function sayGoodbye(name: string): string {
   return `Goodbye, ${name}!`;
 }
+
+
+
+export function greetFormally(name: string): string {
+  return `Greetings, ${name}.`;
+}
+
+export function shoutHello(name: string): string {
+  return `HELLO, ${name.toUpperCase()}!`;
+}
